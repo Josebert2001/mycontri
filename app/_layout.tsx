cf0d62451ed1759b84af721510975fc41b7dc35a
Josebert2001 <66714961+Josebert2001@users.noreply.github.com>
2025-06-06 19:49:14 -0700

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout() {
     <>
       <Stack screenOptions={{ headerShown: false }}>
         {session ? (
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)\" options={{ headerShown: false }} />
         ) : (
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         )}
